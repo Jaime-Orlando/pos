40b6993587898390de1de6c356a1aa984258d036
Jaime-Orlando <106204626+Jaime-Orlando@users.noreply.github.com>
2022-05-25 11:02:46 -0500

    (I) Controller: Adding ExpenseController with RestController annotation.

---
+++ pos/src/main/java/com/jaime/pos/ExpenseController.java
@@ -0,0 +1,16 @@
+package com.jaime.pos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController
+{
+
+    @GetMapping("/expense")
+    public String init()
+    {
+        return "Expense Controller is working";
+    }
+
+}
