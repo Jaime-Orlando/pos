47786a7eedb7704115dd513be0eb5ba714b1e09e
Jaime-Orlando <106204626+Jaime-Orlando@users.noreply.github.com>
2022-05-25 15:16:46 -0500

    (I) Controller: Adding ClientController with RestController annotation.
    (R) Controller: Giving the correct name to the SaleController.

--- pos/src/main/java/com/jaime/pos/ClientController.java
+++ pos/src/main/java/com/jaime/pos/ClientController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Sale
+public class ClientController
 {
 
-    @GetMapping(value = "sale")
+    @GetMapping(value = "/client")
     public String init()
     {
-        return "Sale Controller Working!";
+        return "Client Controller Working!!!";
     }
 
 }
