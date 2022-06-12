e71cad708d5c91a3ac08adae619fda5740fc7d67
Jaime-Orlando <106204626+Jaime-Orlando@users.noreply.github.com>
2022-06-11 23:19:39 -0500

    (I) Test: Configuring standalone app test for UserRestController.

--- pos/src/test/java/com/jaime/pos/controller/api/UserRestControllerTest.java
+++ pos/src/test/java/com/jaime/pos/controller/api/UserRestControllerTest.java
@@ -1,36 +1,83 @@
 package com.jaime.pos.controller.api;
 
+import com.jaime.pos.model.UserModel;
+import com.jaime.pos.service.UserServiceI;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.containsString;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-public class UserRestControllerTest
-{
-    MockMvc mockMvc;
+@ExtendWith(MockitoExtension.class)
+public class UserRestControllerTest {
+    private MockMvc mockMvc;
+    @Mock
+    UserServiceI userService;
 
     @BeforeEach
-    public void setup(WebApplicationContext context)
-    {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new UserRestController(userService)).build();
+        List<UserModel> userModels = findAll();
+        when(userService.findAll()).thenReturn(userModels);
     }
 
     @Test
-    public void testList() throws Exception {
-        mockMvc.perform(get("/user/list/running"))
-                .andDo(print())
+    public void create() {
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/list")
+                        .param("page", "1")
+                        .param("pageSize", "50")
+                        .param("sortDesc", "asc"))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(status()
                         .isOk())
                 .andExpect(content()
-                        .string(containsString("User OK")));
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        JSONObject response = new JSONObject(result.getResponse().getContentAsString());
+        JSONArray items = response.getJSONArray("items");
+        assertEquals(1, items.length());
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    private List<UserModel> findAll() {
+        List<UserModel> users = new ArrayList<>();
+        UserModel user = new UserModel();
+        user.setId(2);
+        user.setName("ben.freddy");
+        user.setLastAccess(LocalDateTime.now());
+        user.setRoleId(2);
+        users.add(user);
+        return users;
     }
 }
